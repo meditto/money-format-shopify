6a235c027f25a1418346d47a113ae6a956964904
Med Djelaili <meddjelaili.me@gmail.com>
2024-05-23 16:44:35 +0100

    chore: Refactor formatMoney function to use Decimal.js library

--- dist/index.js
+++ dist/index.js
@@ -1,10 +1,5 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const decimal_js_1 = __importDefault(require("decimal.js"));
-function formatMoney(amount, format) {
+import Decimal from 'decimal.js';
+export default function formatMoney(amount, format) {
     return format
         .replace(/{{\s*amount\s*}}/g, apply_separators({ amount }))
         .replace(/{{\s*amount_no_decimals\s*}}/g, apply_separators({ amount, round: true }))
@@ -18,10 +13,9 @@ function formatMoney(amount, format) {
         .replace(/{{\s*amount_with_space_separator\s*}}/g, apply_separators({ amount, thousandsSeparator: ' ', fractionalSeparator: ',' }))
         .replace(/{{\s*amount_with_period_and_space_separator\s*}}/g, apply_separators({ amount, thousandsSeparator: ' ' }));
 }
-exports.default = formatMoney;
 function apply_separators({ amount, fractionalSeparator = '.', thousandsSeparator = ',', round = false }) {
     // we need to get the decimal part of the number
-    const decimal = new decimal_js_1.default(round ? Math.round(amount) : amount);
+    const decimal = new Decimal(round ? Math.round(amount) : amount);
     const integerPart = decimal.trunc().toString();
     const precision = decimal.decimalPlaces();
     const fractionalPart = decimal
